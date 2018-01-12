a500ef9b0db85ecee86e12d76dc87f041bc53189
jennitz <35234535+jennitz@users.noreply.github.com>
2018-01-12 07:10:16 -0500

    Add files via upload

---
+++ KangJavaScriptExercises/script.js
@@ -0,0 +1,17 @@
+//exercise1
+var entry = "Please enter a number:"
+var sum = 0;
+var num = prompt(entry);
+for (var i=1; i<=num; i++) {
+  sum += i;
+}
+alert("The sum of all numbers leading up to and including "+num+" is: "+sum);
+console.log(sum);
+
+//exercise2
+
+
+
+//exercise3
+
+//exercise4
